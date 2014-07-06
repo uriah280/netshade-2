bb8aaa9344953f470b517d642ed99170c1e5121c
miltonejones@gmail.com <miltonejones@gmail.com@927b1945-cf97-db78-bd6f-7707caaaba70>
2014-07-06 12:40:23 +0000

    more path changes

--- trunk/netshade/public/index.php
+++ trunk/netshade/public/index.php
@@ -43,7 +43,7 @@ function trunc($a, $b=50) {
 function cmp($a, $b) { 
     return (nameOf($a->subject) < nameOf($b->subject)) ? -1 : 1;
 }
-define ("HOME_PATH", "/home/sa/webroot/");
+define ("HOME_PATH", "/home/ubuntu/webroot/");
 define ('PAGE_PATTERN', "<a href='%s%s'>%s</a>");
 define ('LOGFILE', HOME_PATH . "netshade/ShadeDb.log");
 define ('PAGE_MAX', 10);
